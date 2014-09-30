e4216c0004a244a1a15aa1ca37335f536aaf1566
Ivairpuerari <ivaair@hotmail.com>
2014-09-30 17:49:04 -0300

    Create class Variavel.java

---
+++ Fonte/Variavel.java
@@ -0,0 +1,21 @@
+class Variavel{
+	private String nome;
+	private String valor;
+
+	public void setNome(String n){
+		this.nome = n;
+	}
+
+	public void setValor(String v){
+		this.valor = v;
+	}
+	
+	public String getNome(){
+		return this.nome;
+	}
+	
+	public String getValor(){
+		return this.valor;
+	}
+
+}
