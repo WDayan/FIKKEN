224d75a285032a534c37e3f542e9c69b5ec683ce
schein00 <schein.jefer@gmail.com>
2014-09-30 16:42:24 -0300

    criacao da classe imprime, que sera usada para comandos de saida

---
+++ Fonte/Imprime.java
@@ -0,0 +1,20 @@
+class Imprime{
+	private String impresao;
+
+	public void setImp(String a){
+		this.impresao=a;
+	}
+	
+	public String getImp(){
+		return this.impresao;
+	}
+
+	public void setIres(String a){
+		this.impresao = a;
+	}
+
+	public String getIres(){
+		return this.impresao;
+	}
+
+}
