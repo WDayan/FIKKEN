3fa691410c074a43ed1082b516644d01f2058abc
schein00 <schein.jefer@gmail.com>
2014-10-01 16:06:24 -0300

    Criacao de um vetor de variaveis, metodo que separa cada linha e manda para o metodo tokens para fazer a verificacao dos tokens da linha

--- Fonte/Interpretador.java
+++ Fonte/Interpretador.java
@@ -1,5 +1,21 @@
 public class Interpretador{
 	Variavel var[] = new Variavel[50];
 
+	public void interpreta(String c[]){
+		int i;
+
+		for(i = 0; i < c.length; i++){
+			if(c[i] != null){
+				this.tokens(c[i])
+			}
+
+		}
+
+	}
+
+	public void tokens(string c){
+
+
+	}
 
 }
