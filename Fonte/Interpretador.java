fc3ef3d9aa8299630ebbfd9ea657f41abebbdd98
Ivairpuerari <ivaair@hotmail.com>
2014-10-14 14:25:29 -0300

    parte de implementação do metodo interpreta();

--- Fonte/Interpretador.java
+++ Fonte/Interpretador.java
@@ -9,6 +9,54 @@ import java.util.Scanner;
 
 public class Interpretador{
 
-	public void interpreta(String com[]){}
+
+
+
+
+
+	public void interpreta(String com[]){
+			 int i;
+			 char operador;
+			 string comando;
+			for(i=0; i<com.length && com[i] != null; i++){
+				com[i]=com[i].trim();
+				com[i]=com[i].replace("se","*");
+				com[i]=com[i].replace("fimse",".");
+				com[i]=com[i].replace("var","^");
+				com[i]=com[i].replace("while","@");
+				com[i]=com[i].replace("imprime","-");
+				com[i]=com[i].replace("break","!");
+		
+		}
+
+		for(i=0; i<com.length && com[i] != null; i++){
+							
+			//Retira o token e salva na instrucao
+			operador=com[i].charAt(0);
+			comando=com[i].substring(1);
+			comando=comando.trim();
+
+			switch(operador){
+
+				case '-' : this.imprime(comando);
+
+	}
+
+
+<<<<<<< .mine
+	}
+
+	public void tokens(string c){
+		String file[] = a.split(" ");
+		int i;
+
+		for(i = 0; i < file.length; i++){
+			if (File [i] == '$var' && File[i+1] == '='  ) {
+				   
+			}
+		}
+
+	}
+
 
 }
