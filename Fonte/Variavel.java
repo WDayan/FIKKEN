1f598185dfdf512c9e672c2eed6b24c3348d54d9
schein00 <schein.jefer@gmail.com>
2014-11-02 23:18:46 -0200

    substituicao da classe Imprime.java por Imp.java, que usa metodo statico retirada de alguns metodos da classe Variavel.java e criacao do construtor e criacao da classe VarU.java que é usadada para gerenciar as variaeis do sistema

--- Fonte/Variavel.java
+++ Fonte/Variavel.java
@@ -1,21 +1,29 @@
 /*
 *
+*	Trabalho de programacoa 1
+*
+*	Feito por: 
+*	Jeferson A. Schein
+*	e-mail : schein.jefer@gmail.com
+*	Ivair Puerari
+*	e-mail : ivaair@hotmail.com
 *
 *
-*	Classe usada para declarar variaveis, que podem ser inicializadas sem nome nem valor.
+*	Classe usada para declarar variaveis, 
+*	que quando instanciada colocara uma String no nome e um double no valor.
+*	A classe tb possui metodos para alterar nome e valores.
 *	As variaveis so podem ser do tipo double.
 *
 */
 
-
 class Variavel{
 	private String nome;
 	private double valor;
-
+	
 	public void setNome(String n){
 		this.nome = n;
 	}
-
+	
 	public void setValor(double v){
 		this.valor = v;
 	}
@@ -28,15 +36,9 @@ class Variavel{
 		return this.valor;
 	}
 
-	public Boolean verificaVar(String a, Variavel var[] ){
-		int i;
-		Boolean valor = false;
-		for (i = 0 ; i < var.length ; i++ ) {
-			 if((var[i].nome).equals(a)); valor = true;			
-		}
-
-			return valor;
-
-
+	public Variavel(String s, double d){
+		this.nome = s;
+		this.valor = d;
 	}
+	
 }
