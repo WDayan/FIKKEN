2f72954579fd5d10b34677380ca468001263f718
Ivairpuerari <ivaair@hotmail.com>
2014-11-03 10:49:00 -0200

    Update Logico.java
    add metodo funcaSe

--- Fonte/Logico.java
+++ Fonte/Logico.java
@@ -1,5 +1,33 @@
 class Logico{
-	
+	public static boolean funcaoSe(VarU var[],String a){
+		int i;
+		String[] teste = a. split("\\>|\\<|\\!|\\&");
+		double cont[] = new double[teste.length];
+
+		for(i = 0 ; ((i < teste.length) && (teste[i] != null)) ; i++){
+			if(VarU.varE(teste[i]){
+				cont[i]= VarU.valorVar(teste[i]);
+			}
+			else{
+				cont[i]=Double.parseDouble(teste[i]);
+			}
+		}
+		for(i = 0 ; i < a.length() ; i++){
+			if(a.charAt(i) == '>' || a.charAt(i) == '<' || a.charAt(i) == '&' || a.charAt(i) == '!'){
+				switch(a.charAt(i)){
+					case '>': 
+					 	return cont[0]>cont[1];
+					case '<': 
+					 	return cont[0]<cont[1];
+					case '&': 
+						return cont[0]==cont[1];
+					case '!':
+						  if(cont[0]>cont[1] || connt[0]<cont[1]) return True; 				}		
+				}
+			}	
+		}
+		return false;
+	}
 
 	
 }
